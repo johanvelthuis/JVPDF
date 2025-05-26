91818e070297ac7e74b23a784ab7447b1ffe87f7
Johan Velthuis <83608208+johanvelthuis@users.noreply.github.com>
2025-05-26 12:25:33 +0200

    Fix UnitConverter tests for coordinate precision

---
+++ tests/UnitConverterTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace JVelthuis\JVPdf\Tests;
+
+use PHPUnit\Framework\TestCase;
+use JVelthuis\JVPdf\UnitConverter;
+use JVelthuis\JVPdf\Unit;
+
+class UnitConverterTest extends TestCase
+{
+    public function testConvertUnit()
+    {
+        $this->assertEquals(10, UnitConverter::convertUnit(10, Unit::POINT));
+        $this->assertEqualsWithDelta(28.346, UnitConverter::convertUnit(10, Unit::MM), 0.001);
+        $this->assertEquals(720, UnitConverter::convertUnit(10, Unit::INCH));
+    }
+
+    public function testConvertFromPoints()
+    {
+        $this->assertEquals(10, UnitConverter::convertFromPoints(10, Unit::POINT));
+        $this->assertEqualsWithDelta(3.528, UnitConverter::convertFromPoints(10, Unit::MM), 0.001);
+        $this->assertEqualsWithDelta(0.1389, UnitConverter::convertFromPoints(10, Unit::INCH), 0.001);
+    }
+
+    public function testConvertCoordinates()
+    {
+        $pageSize = [200, 300];
+
+        $result = UnitConverter::convertCoordinates(10, 15, $pageSize, Unit::POINT, false);
+        $this->assertEquals([10, 15], $result);
+
+        $result = UnitConverter::convertCoordinates(10, 15, $pageSize, Unit::POINT, true);
+        $this->assertEquals([10, 285], $result);
+
+        $pageSize = [595, 842];
+        $result = UnitConverter::convertCoordinates(25.4, 25.4, $pageSize, Unit::MM, true);
+        $this->assertEqualsWithDelta(72, $result[0], 0.001);
+        $this->assertEqualsWithDelta(770, $result[1], 0.001);
+    }
+}
